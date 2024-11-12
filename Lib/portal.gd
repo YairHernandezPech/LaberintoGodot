9ee4b33aab20305793ec4ab71eb3185c9eb08f26
YairHernandezPech <edilsonchelitohernandez011@gmail.com>
2024-11-11 20:28:10 -0600

    cambios

--- Lib/portal.gd
+++ Lib/portal.gd
@@ -8,5 +8,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("Tocando el portal")
 		body.queue_free()
-		get_tree().change_scene_to_file("res://Lib/menu.tscn")
+		get_tree().change_scene_to_file("res://Lib/mundo2/Mundo2.tscn")
 	pass # Replace with function body.
